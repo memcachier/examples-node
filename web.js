e886242f68c0785ec04fb03fd290de274a0c31c8
Amit Aryeh Levy <amit@amitlevy.com>
2012-05-30 00:20:50 -0700

    Add memjs

--- web.js
+++ web.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var MemJS = require("memjs").Client
+
+memjs = MemJS.create();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  memjs.get("latest", function(value) {
+    if (value) {
+      response.send(value.toString());
+    } else {
+      response.send('No value yet');
+    }
+  })
+});
+
+app.get('/set/:value', function(request, response) {
+  memjs.set("latest", request.params.value);
+  response.redirect('/');
 });
 
 var port = process.env.PORT || 3000;
